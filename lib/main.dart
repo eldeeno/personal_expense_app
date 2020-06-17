cada0e33f7028cb68bd7fed58f165e49f275e025
Shamsuddeen Abdulkadir <ibnabdulkadirb@gmail.com>
2020-06-17 15:28:35 +0100

    added transactions sample & basic home layout.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'models/transaction.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,14 +14,50 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+      id: 't1',
+      title: 'Nike Shoes',
+      amount: 2500,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Marks and spencer shirt',
+      amount: 7000,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't3',
+      title: 'Versace Watch',
+      amount: 25000,
+      date: DateTime.now(),
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Flutter App'),
-      ),
-      body: Center(child: Text('Widget Playground'),)
-    );
+        appBar: AppBar(
+          title: Text('Flutter App'),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              width: double.infinity,
+              child:
+                  Card(color: Colors.blue, child: Text('Chart!'), elevation: 5),
+            ),
+            Column(
+              children: transactions.map((tx) {
+                return Card(
+                  child: Text(tx.title),
+                );
+              }).toList(),
+            ),
+          ],
+        ));
   }
 }
