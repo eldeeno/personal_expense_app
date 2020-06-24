071cf80c9491b8c6229bbfe7491bd1df25de10bc
Shamsuddeen Abdulkadir <ibnabdulkadirb@gmail.com>
2020-06-24 22:42:33 +0100

    Fixed APp title

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter App',
+      title: 'Personal Expenses',
       home: MyHomePage(),
     );
   }
@@ -72,7 +72,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text('Flutter App'),
+          title: Text('Personal Expenses'),
           actions: <Widget>[
             IconButton(
                 icon: Icon(Icons.add), onPressed: () => _showAddNewTx(context)),
